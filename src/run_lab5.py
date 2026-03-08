2f1f1e3c3ad271ccf2c38faa25da39b111ae4ca6
joshuacarlmari <joshuacarlmarifa@gmail.com>
2026-03-08 15:40:39 +0800

    Created Structured Program Execution

--- src/run_lab5.py
+++ src/run_lab5.py
@@ -1,3 +1,11 @@
 import json
 from spatial import Parcel
 
+import json
+from spatial import Parcel
+import analysis
+
+
+def load_parcels(filepath):
+    with open(filepath, "r") as f:
+        data = json.load(f)
--- src/spatial.py
+++ src/spatial.py
@@ -1,6 +1,10 @@
 from shapely.geometry import shape
 
 class SpatialObject:
+    def __init__(self, geometry):
+        self.geometry = shape(geometry)
+
+
     def effective_area(self):
         """"
         Return the spatial area representation of the object.
@@ -13,10 +17,19 @@ class Parcel(SpatialObject):
         return self.geometry.area
     
 class Building(SpatialObject):
+    def __init__(self, geometry, floors):
+        super().__init__(geometry)
+        self.floors = floors
+
     def effective_area(self):
         return self.geometry.area * self.floors
     
 class Road(SpatialObject):
+    def __init__(self, geometry, width):
+        super().__init__(geometry)
+        self.width = width
+
+
     def effective_area(self):
         return self.geometry.buffer(self.width).area
 
